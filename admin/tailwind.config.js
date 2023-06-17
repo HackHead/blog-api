152cded35f9b94114663f1cd66e2e63b1e06be08
Andrew <SolomonAndreew@tutanota.com>
2023-06-17 19:40:30 +0300

    feat: init admin react application

---
+++ admin/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./main.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
